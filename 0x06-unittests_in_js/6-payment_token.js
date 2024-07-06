785a3dde6fbc260976b602bebc1b79c6e1226a70
mburuxx <njihiamburu.3@gmail.com>
2024-07-06 10:45:02 +0300

    Async tests with done

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,12 +1,7 @@
-function getPaymentTokenFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ data: 'Successful response from the API' });
-    } else {
-      // If success is false, the function does nothing (no rejection).
-      // You might consider rejecting with an appropriate error in a real scenario.
-    }
-  });
-}
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
+  if (success) {
+    resolve({data: 'Successful response from the API'});
+  }
+});
 
-module.exports = { getPaymentTokenFromAPI };
+module.exports = getPaymentTokenFromAPI;
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,25 +1,11 @@
-const { getPaymentTokenFromAPI } = require('./6-payment_token');
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  test('should resolve with correct data when success is true', (done) => {
+  it('getPaymentTokenFromAPI(success), where success == true', (done) => {
     getPaymentTokenFromAPI(true)
-      .then((response) => {
-        expect(response).toEqual({ data: 'Successful response from the API' });
-        done();
-      })
-      .catch((error) => {
-        done(error); // Fail the test if there's an error
-      });
-  });
-
-  test('should not resolve when success is false', (done) => {
-    getPaymentTokenFromAPI(false)
-      .then((response) => {
-        // This test should not resolve, so we should not reach here
-        done(new Error('Expected promise to be rejected, but it was resolved with: ' + JSON.stringify(response)));
-      })
-      .catch(() => {
-        // Expected behavior: promise should be rejected
+      .then((res) => {
+        expect(res).to.deep.equal({data: 'Successful response from the API'});
         done();
       });
   });
