db976e34ca9434ec09f5e8fac10e94235a429e04
mburuxx <njihiamburu.3@gmail.com>
2024-07-06 09:51:02 +0300

    Fix error

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,63 +1,41 @@
 const fs = require('fs');
 
-// Polyfill for Object.fromEntries
-if (!Object.fromEntries) {
-    Object.fromEntries = function(entries) {
-        if (!entries || !Array.isArray(entries)) {
-            throw new TypeError('Object.fromEntries requires an array-like object as an argument');
-        }
-        const obj = {};
-        for (let i = 0; i < entries.length; i++) {
-            const [key, value] = entries[i];
-            obj[key] = value;
-        }
-        return obj;
-    };
-}
-
 /**
  * Counts the number of students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
+ * @returns {Promise} A promise that resolves when the operation is complete.
  */
 const countStudents = (dataPath) => new Promise((resolve, reject) => {
-    fs.readFile(dataPath, 'utf-8', (err, data) => {
-        if (err) {
-            reject(new Error('Cannot load the database'));
-        }
-        if (data) {
-            const fileLines = data
-                .toString('utf-8')
-                .trim()
-                .split('\n');
-            const studentGroups = {};
-            const dbFieldNames = fileLines[0].split(',');
-            const studentPropNames = dbFieldNames
-                .slice(0, dbFieldNames.length - 1);
+  fs.readFile(dataPath, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
 
-            for (const line of fileLines.slice(1)) {
-                const studentRecord = line.split(',');
-                const studentPropValues = studentRecord
-                    .slice(0, studentRecord.length - 1);
-                const field = studentRecord[studentRecord.length - 1];
-                if (!Object.keys(studentGroups).includes(field)) {
-                    studentGroups[field] = [];
-                }
-                const studentEntries = studentPropNames
-                    .map((propName, idx) => [propName, studentPropValues[idx]]);
-                studentGroups[field].push(Object.fromEntries(studentEntries));
-            }
+    const fileLines = data.trim().split('\n').filter(line => line.length > 0); // Filter out empty lines
+    const studentGroups = {};
+    const dbFieldNames = fileLines[0].split(',');
+    const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
-            const totalStudents = Object
-                .values(studentGroups)
-                .reduce((pre, cur) => (pre || []).length + cur.length, 0);
-            console.log(`Number of students: ${totalStudents}`);
-            for (const [field, group] of Object.entries(studentGroups)) {
-                const studentNames = group.map((student) => student.firstname).join(', ');
-                console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
-            }
-            resolve(true);
-        }
-    });
+    for (const line of fileLines.slice(1)) {
+      const studentRecord = line.split(',');
+      const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+      const field = studentRecord[studentRecord.length - 1];
+      if (!Object.keys(studentGroups).includes(field)) {
+        studentGroups[field] = [];
+      }
+      const studentEntries = studentPropNames.map((propName, idx) => [propName, studentPropValues[idx]]);
+      studentGroups[field].push(Object.fromEntries(studentEntries));
+    }
+
+    const totalStudents = Object.values(studentGroups).reduce((pre, cur) => pre + cur.length, 0);
+    console.log(`Number of students: ${totalStudents}`);
+    for (const [field, group] of Object.entries(studentGroups)) {
+      const studentNames = group.map(student => student.firstname).join(', ');
+      console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+    }
+    resolve();
+  });
 });
 
 module.exports = countStudents;
