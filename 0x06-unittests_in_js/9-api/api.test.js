b0787a3da01d535bf4b20ef33edd7d8b9253e0bd
mburuxx <njihiamburu.3@gmail.com>
2024-07-06 12:04:37 +0300

    Fix errors

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,20 +1,45 @@
-const express = require('express');
+const request = require('request');
+const { expect } = require('chai');
 
-const app = express();
-const PORT = 7865;
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-app.get('/', (_, res) => {
-  res.send('Welcome to the payment system');
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  describe('GET /cart/:id', () => {
+    it('returns correct response when :id is a number', (done) => {
+      const validId = 123;
+      request.get(`${API_URL}/cart/${validId}`, (_err, res, body) => {
+        expect(res.statusCode).to.be.equal(200);
+        expect(body).to.be.equal(`Payment methods for cart ${validId}`);
+        done();
+      });
+    });
+
+    it('returns 400 status code when :id is NOT a number', (done) => {
+      const invalidId = 'abc';
+      request.get(`${API_URL}/cart/${invalidId}`, (_err, res, body) => {
+        expect(res.statusCode).to.be.equal(400);
+        const responseBody = JSON.parse(body);
+        expect(responseBody.errors).to.be.an('array').that.is.not.empty;
+        expect(responseBody.errors[0]).to.have.property('msg').that.equals('ID must be a number');
+        done();
+      });
+    });
+
+    it('returns correct response when :id is a string number', (done) => {
+      const stringNumberId = '456';
+      request.get(`${API_URL}/cart/${stringNumberId}`, (_err, res, body) => {
+        expect(res.statusCode).to.be.equal(200);
+        expect(body).to.be.equal(`Payment methods for cart ${stringNumberId}`);
+        done();
+      });
+    });
+  });
 });
-
-app.get('/cart/:id(\\d+)', (req, res) => {
-  const { id } = req.params;
-
-  res.send(`Payment methods for cart ${id}`);
-});
-
-app.listen(PORT, () => {
-  console.log(`API available on localhost port ${PORT}`);
-});
-
-module.exports = app;
